f3d5341f75f2383ba1c425cedbcc0135b76990ed
Rade <jrrade@iastate.edu>
2025-04-21 19:32:18 -0500

    Add Pstructured response handling in main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langchain.schema.messages import SystemMessage, HumanMessage
+from pydantic import BaseModel, Field
+from langchain.output_parsers.structured import StructuredOutputParser
 
 AVAILABLE_MODELS = ['gpt4o', 'claude3', 'gemini']
 API_KEYS = {
@@ -35,7 +37,19 @@ def get_model(model):
     elif model == 'gemini':
         return ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=api_key)
 
+class AFMResponse(BaseModel):
+    answer: str = Field(description="A breif and technical answer to the AFM question.")
+    recommendations: list[str] = Field(description="Practical steps or suggestions.")
+    
 def main(model_name):
+    # List of AFM-related questions from the benchmark questions
+    questions = []
+    with open('./benchmark_questions/AFM_LLM_Combined_50_Questions.json', 'r') as f:
+        questions_data = json.load(f)
+
+    for question in questions_data:
+        questions.append(question['question'])
+
     if model_name not in AVAILABLE_MODELS:
         raise ValueError(f"Invalid model. Available models: {AVAILABLE_MODELS}")
 
@@ -46,42 +60,46 @@ def main(model_name):
 
     # Initialize models
     model = get_model(model_name)
+    
     # Expert system prompt
     # system_prompt = SystemMessage(
     #     content="You are an expert in Atomic Force Microscopy (AFM). You provide precise and insightful answers about AFM image defects,\
     #             scanning parameters, biological samples, and tip contamination. Keep responses concise and technical.")
 
     system_prompt = SystemMessage(
-        content="You are an expert in atomic force microscopy (AFM). You are given a type of defect (Tip Contamination or Not Tracking) in AFM image.\
-                You need to help the user understand the defect and how to avoid it. Give concise and correct answer for the user questions.\
-                ")
+        content="You are an expert in atomic force microscopy (AFM). You are given a type of defect (Tip Contamination or Not Tracking) in AFM image. You need to help the user understand the defect and how to avoid it. Give brief, technical and correct answer for the user questions.")
                 #I have compiled the questions from AFM users and I want you to answer them.        
 
-    # List of AFM-related questions from the benchmark questions
-    questions = []
-    with open('./benchmark_questions/AFM_LLM_Combined_50_Questions.json', 'r') as f:
-        questions_data = json.load(f)
-
-    for question in questions_data:
-        questions.append(question['question'])
-
+    # Define the output parser
+    # parser = StructuredOutputParser(pydantic_schema=AFMResponse)
+    
     # Placeholder for responses
     results = []
 
     # Loop through each question and get responses from each model
     for q in questions[:5]:
-        user_msg = HumanMessage(content=q)
+        # format_instructions = parser.get_format_instructions()
+        # prompt = f"""
+        # {format_instructions}
+        # Question: {q}
+        # """
+        prompt = q
+        user_msg = HumanMessage(content=prompt)
         messages = [system_prompt, user_msg]
 
         # Get responses
         print(f"\n🧪 Question: {q}\n{'='*80}")
-        model_resp = model.invoke(messages).content
+        model_with_structured_output = model.with_structured_output(AFMResponse)
+        model_resp = model_with_structured_output.invoke(messages)
+        # model_resp = model.invoke(messages).content
+        # model_resp = parser.parse(model_resp)
         print(f"\n[{model_name}]:\n{model_resp}")
         # Save to list
         results.append({
             "Question": q,
             # "Model": model_name,
-            "Answer": model_resp
+            "Answer": model_resp.answer,
+            "Recommendations": model_resp.recommendations
         })
 
     # Export to JSON instead of CSV
